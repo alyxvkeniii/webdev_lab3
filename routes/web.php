304e2483edb63695b5ea55dfa67e75ae7484e802
keniiiiiii <kenidumb112@gmail.com>
2024-09-12 18:11:15 +0800

    Upload files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMe;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/content', function () {
+    return view('content');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::post('/contact', function () {
+    $data = request()->all();
+    Mail::to('mingkai103019@gmail.com')->send(new ContactMe($data));
+    return redirect('/contact')->with('flash', 'Message Sent Successfully');
 });
